cecaf4b8608e1fcfa0f61872e25d9d2ff7217060
michael <michaelzhou20111@hotmail.com>
2025-11-17 11:21:18 -0500

    added contact links and headers

---
+++ src/Home.jsx
@@ -0,0 +1,27 @@
+import './Home.css'
+
+function Home() {
+  return (
+    <div>
+      <h1>Michael Zhou</h1>
+      <h2>CS & Stats @ University of Toronto</h2>
+      <h3>Image of Me</h3>
+      <div class='sections'>
+        <p>About</p>
+        <p>Work Experience</p>
+        <p>Projects</p>
+        <p>Writing</p>
+
+      </div>
+
+      <div class='contacts'>
+        <a href='https://linkedin.com/in/michaelz04' target="_blank">LinkedIn</a>
+        <a href='https://github.com/michaelz04/' target="_blank">GitHub</a>
+        <a href="/Michael_Resume.pdf" target="_blank">Resume</a>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Home
