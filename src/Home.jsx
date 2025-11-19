34781d0154166ebee9ff3be805df0e2b44f4f475
michael <michaelzhou20111@hotmail.com>
2025-11-18 19:47:02 -0500

    added work history and dynamic colour background

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,50 @@
 import './Home.css'
 
 function Home() {
+
+  const workHistory = [
+    {
+      role: "Incoming Software Engineer Intern @ AMD",
+      dateRange:"May 2026 - Aug 2027"
+    },
+    {
+      role: "IT Business Analyst @ Intact",
+      dateRange:"Sept 2025 - Dec 2025"
+    },
+    {
+      role: "Software Developer @ Toromont CAT",
+      dateRange:"Sept 2024 - Dec 2024"
+    },
+    {
+      role: "Data Analyst @ Toromont CAT",
+      dateRange:"Jan 2024 - Apr 2024"
+    }
+  ];
   return (
-    <div>
+    <div className="dynamic-background"> 
       <h1>Michael Zhou</h1>
       <h2>CS & Stats @ University of Toronto</h2>
+      <hr className="divider" />
       <h3>Image of Me</h3>
       <div class='sections'>
         <p>Hey! I'm Michael.</p>
-        <h4>Work Experience</h4>
-        <h4>Projects</h4>
-        <h4>Writing</h4>
+        <hr className="subtle-divider" />
+        <h3>Work Experience</h3>
+        <div class='work-items'>
+          {
+            workHistory.map((job, index) => (
+              <div key={index}>
+                <p className="box">{job.role}</p>
+                <p className="box">{job.dateRange}</p>
+              </div>
+            
+            ))
+          }
+        </div>
+        
+        <hr className="subtle-divider" />
+        <h3>Projects</h3>
+        <h3>Writing</h3>
 
       </div>
 
