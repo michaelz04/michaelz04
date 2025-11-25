50edf9e7920cf4854ed17057e63c6b3bee506f8d
michael <michaelzhou20111@hotmail.com>
2025-11-24 19:06:53 -0500

    added a sentence for about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,8 +6,7 @@ function About() {
     <div> 
       <Header></Header>
         <h3>About</h3>
-        <p>Hey! I'm Michael</p>
-        
+        <p class="box">Hey! I'm Michael. I'm a 4th year student at the University of Toronto, majoring in computer science and statistics.</p>
     </div>
   )
 }
